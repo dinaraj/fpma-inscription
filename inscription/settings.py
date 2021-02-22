c61378bd318483d7655401d8a7162d5cc42727bd
Dina Rajaonson <d.rajaonson@gmail.com>
2021-02-22 08:28:36 +0400

    optimisation front + logo

--- inscription/settings.py
+++ inscription/settings.py
@@ -126,6 +126,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 MESSAGE_TAGS = {
     message_constants.DEBUG: 'debug',
     message_constants.INFO: 'info',
